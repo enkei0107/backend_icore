e651654cdebf4f438565c45f295fbff213c4d5c2
deny andriana <deny.andriana@rakhasa.com>
2023-12-20 13:39:28 +0700

    feat(user adddress): user address

--- src/front-office/user-address/user-address.service.ts
+++ src/front-office/user-address/user-address.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserAddressDto } from './dto/create-user-address.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAddress } from './entities/user-address.entity';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Users } from '../user/entities/user.entity';
+import { CreateUserAddressDto } from './dto/create-user-address.dto';
 
 @Injectable()
 export class UserAddressService {
   constructor(
     @InjectRepository(UserAddress)
     private userAddressRepository: Repository<UserAddress>,
-  ) { }
-  create(createUserAddressDto: CreateUserAddressDto) {
-    return 'This action adds a new userAddress';
+  ) {}
+
+  async updateOrCreate(
+    user: Users,
+    userAddressDto: CreateUserAddressDto,
+  ): Promise<UserAddress> {
+    const result = await this.userAddressRepository.findOneBy({
+      user_id: user.id,
+    });
+
+    if (result) {
+      await this.userAddressRepository.update(
+        { user_id: user.id },
+        userAddressDto,
+      );
+      return await this.userAddressRepository.findOneBy({ user_id: user.id });
+    } else {
+      const newAddress = this.userAddressRepository.create({
+        ...userAddressDto,
+        user: user,
+      });
+      return await this.userAddressRepository.save(newAddress);
+    }
   }
 }
