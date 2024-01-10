1268d1a4e2a4e3408d651a66e9078bf1297a455c
deny andriana <deny.andriana@rakhasa.com>
2024-01-10 13:36:16 +0700

    fix(seeder unique): role uqnique enum

--- src/database/factories/role.factory.ts
+++ src/database/factories/role.factory.ts
@@ -10,9 +10,19 @@ import { UserAccountTypeEnum } from 'src/config/enum/user/user-account-type.enum
 export class RolesFactory extends Factory<Roles> {
   protected entity = Roles;
   protected dataSource = dataSource;
+  private roleUnique(): string {
+    const data = faker.helpers.enumValue(UserAccountTypeEnum);
+    if (!this.currentRole.includes(data)) {
+      this.currentRole.push(data);
+      return data;
+    }
+    return this.roleUnique();
+  }
+  protected currentRole = [];
+
   protected attrs(): FactorizedAttrs<Roles> {
     return {
-      name: faker.helpers.enumValue(UserAccountTypeEnum),
+      name: this.roleUnique(),
     };
   }
 }
